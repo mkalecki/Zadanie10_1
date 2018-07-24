8cbe75d29783493753ab055ef1e5a552deefab07
Michał Kalecki <mkalecki@gmail.com>
2018-07-25 00:09:18 +0200

    Zadanie10_1 wyjątki kontrolowane jakoś obsłużone

--- src/IncorrectAgeException.java
+++ src/IncorrectAgeException.java
@@ -1,4 +1,4 @@
-public class IncorrectAgeException extends RuntimeException{
+public class IncorrectAgeException extends Exception{
     public IncorrectAgeException(String message){
         super(message);
     }
